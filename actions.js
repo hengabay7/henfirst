240c10ba9db92a6784ad281cdb61fb14d21511bc
hengabay7 <101103416+hengabay7@users.noreply.github.com>
2022-03-14 12:05:34 +0200

    hen-14032022

--- actions.js
+++ actions.js
@@ -1,11 +1,9 @@
 const express=require('express');
 const router=express.router();
-
 router.git('/greeting',(req,res)=>{
-    return res.status(5000).json({
+    return res.status(500).json({
         Message:'welcome to me api'
     });
 
-});
-    
+});  
 module.exports=router;
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-console.log('walcome hen gabay');
+const express=require('express');
+const bodyparser=require('body-parser');
+const bodyParser = require('body-parser');
+const app=express
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+const port=5000;
+app.listen(port,function(){
+    console.log('server is working via port ${port}');
+});
 
 
