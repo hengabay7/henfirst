14b15705907e1278da10b251cd18c4fc086472bc
hengabay7 <101103416+hengabay7@users.noreply.github.com>
2022-03-16 10:36:04 +0200

    hen-16032022

--- actions.js
+++ actions.js
@@ -1,12 +1,19 @@
 const express=require('express');
 const router=express.Router();
 
+
+    let users=[];
+
+
 router.post('/register',(req,res)=>{
-    const useremail=req.body.email;
-    const userpassword=req.body.password;
+    //const useremail=req.body.email;
+    //const userpassword=req.body.password;
+    const{email,password}=req.body;
+
+users.push({email:email,password:password});
 
     return res.status(200).json({
-        Message:`Hello ${useremail}`
+        Message:users
     });
 
 })
