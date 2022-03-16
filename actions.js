bf1031e86a0f58c8f96d4b31a63428cc3debe862
hengabay7 <101103416+hengabay7@users.noreply.github.com>
2022-03-16 10:10:57 +0200

    hen-16032022

--- actions.js
+++ actions.js
@@ -1,6 +1,16 @@
 const express=require('express');
 const router=express.Router();
 
+router.post('/register',(req,res)=>{
+    const useremail=req.body.email;
+    const userpassword=req.body.password;
+
+    return res.status(200).json({
+        Message:`Hello ${useremail}`
+    });
+
+})
+
 router.get('/greeting',(req,res)=>{
     return res.status(200).json({
         Message:'welcome to me.js Api'
